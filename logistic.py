358da4b8830618c04dc2f827cb30b20de4647ef7
kmarv <39408256+kmarv@users.noreply.github.com>
2019-09-16 19:57:13 +0300

    Add files via upload

---
+++ logistic.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  9 16:18:48 2019
+
+@author: marvin
+"""
+# Logistic Regression algorithm
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv(r'C:\Users\marvin\.spyder-py3\work\Social_Network_Ads.csv')
+x = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+#splitting dataset into training set and test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+# x_test and train dataset
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+#fitting logistic regression model to training sets
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0, solver = 'liblinear')
+classifier.fit(x_train, y_train)
+
+#predicting the results
+y_pred = classifier.predict(x_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+#visualize the results
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_train, y_train
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min()-1, stop = x_set[:, 0].max() +1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min()-1, stop = x_set[:, 1].max() +1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha= 0.75, cmap = ListedColormap(('red','green')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red','green'))(i), label = j)
+    
+plt.title('Logistic Regression(Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated salary')
+plt.legend()
+
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_test, y_test
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min()-1, stop = x_set[:, 0].max() +1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min()-1, stop = x_set[:, 1].max() +1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha= 0.75, cmap = ListedColormap(('red','green')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red','green'))(i), label = j)
+    
+plt.title('Logistic Regression(Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated salary')
+plt.legend()
---
+++ naive bayes.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 14 13:41:16 2019
+
+@author: marvin
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 23:13:09 2019
+
+@author: marvin
+"""
+
+# naive Bayes Algorithm
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv(r'C:\Users\marvin\.spyder-py3\work\Social_Network_Ads.csv')
+x = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+#splitting dataset into training set and test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
+#fitting KSVM  model to training sets
+from sklearn.naive_bayes import GaussianNB
+classifier  = GaussianNB()
+classifier.fit(x_train, y_train)
+
+#predicting the results
+y_pred = classifier.predict(x_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+#visualize the results
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_train, y_train
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min()-1, stop = x_set[:, 0].max() +1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min()-1, stop = x_set[:, 1].max() +1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha= 0.75, cmap = ListedColormap(('red','green')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red','green'))(i), label = j)
+    
+plt.title('naive bayes(Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated salary')
+plt.legend()
+
+#visualise the results
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_test, y_test
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min()-1, stop = x_set[:, 0].max() +1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min()-1, stop = x_set[:, 1].max() +1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha= 0.75, cmap = ListedColormap(('red','green')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red','green'))(i), label = j)
+    
+plt.title('naive bayes(Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated salary')
+plt.legend()
